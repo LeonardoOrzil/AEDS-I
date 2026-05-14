9a7c8a94426117831cb910aa00c45ace41a75065
Leonardo Orzil <lorzil2008@gmail.com>
2026-05-14 08:09:39 -0300

    Add secondary diagonal matrix program

---
+++ AEDS-I_Lab./lab10_scndry_dagnl_mtrx.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (){
+    int quad[4][4], c, l;
+
+    for(c=0; c<4; c++){
+        for(l=0; l<4; l++){
+            printf("x=%d,y=%d\n",l+1, c+1);
+            scanf("%d", &quad[c][l]);
+        }
+    }
+    for(c=0; c<4; c++){
+        for(l=0; l<4; l++){
+            printf("%d ", quad[c][l]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    c=0;
+    l=3;
+    while(c<4){
+        printf("%d ", quad[c][l]);
+        c++;
+        l--;
+    }
+return 0;
+}
